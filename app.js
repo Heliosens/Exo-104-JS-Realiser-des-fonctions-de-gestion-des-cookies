4b29afe17de3a6e770cc01d524e29af00ad68a29
Sylvie-B <unsolo@hotmail.fr>
2021-12-15 21:43:27 +0100

    function get cookie name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Le cookie expirera dans 24h, exprimées en millisecondes !
 // 1 jour = 86400000 millisecondes.
 
-// Comment créer un cookie, je suis gentil je laisse l'exemple !
+// // Comment créer un cookie, je suis gentil je laisse l'exemple !
 document.cookie = "theme=dark; path=/; domain=localhost; max-age=86400000; samesite=strict";
 console.log(document.cookie);
 
@@ -12,6 +12,7 @@ console.log(document.cookie);
  */
 function getCookies() {
     let cookies = document.cookie.split(';');
+    console.log(cookies);
     return cookies.map(cookie => cookie.trim());
 }
 
@@ -24,8 +25,8 @@ function getCookies() {
 function setCookie(cookieName, cookieValue) {
     // Le cookie doit être valide 2 jours et doit respecter les normes de sécurité contre les failles CSRF.
     // Votre code ici.
+    document.cookie = cookieName + "=" + cookieValue + "; max-age=1728; samesite=strict";
 }
-
 
 /**
  * TODO Cette fonction doit retourner la valeur du nom du cookie passé en paramètre.
@@ -33,14 +34,19 @@ function setCookie(cookieName, cookieValue) {
  */
 function getCookie(cookieName) {
     // Votre code ici.
+    let cookies = document.cookie.split(';');
+    let test = cookies.map(cookieName => cookieName.trim());
+    for(let item of test){
+        let data = item.split('=');
+        if(data[0] === cookieName) {
+            return data[1];
+        }
+    }
 }
-
-
-console.log(getCookies());
 
 // FIXME Test de setCookie()
 setCookie('monCookie', 'maValeur');
 console.log(getCookies());
 
 // FIXME test de getCookie(param)
-console.log(getCookies('monCookie'));
+console.log(getCookie('monCookie'));
